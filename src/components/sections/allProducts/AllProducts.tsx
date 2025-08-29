937a4c3b47e49b456c3eac5954bc5645e464bfb4
fakhtehpanah <fakhipanah@gmail.com>
2025-08-29 21:03:42 +0330

    fetch all products using api

--- src/components/sections/allProducts/AllProducts.tsx
+++ src/components/sections/allProducts/AllProducts.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { getProductsByCategory, getProductsItems } from '@/components/api/product';
+import React, { useEffect, useState } from 'react'
+import ProductCard from '../productCard/ProductCard';
 
 function AllProduct() {
+
+  const [allProducts, setAllProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchAllProductsData = async () => {
+      try {
+        const allProductsData = await getProductsItems();
+        setAllProducts(allProductsData);
+      } catch(err) {
+        console.log(err);
+      }
+    }
+    fetchAllProductsData();
+  })
   return (
-    <div>kkk</div>
+    <div className='p-5'>
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6'>
+        {allProducts.map((product) => (
+        <ProductCard key={product.id} product={product}/>
+      ))}
+      </div>
+      
+
+
+    </div>
   )
 }
 
