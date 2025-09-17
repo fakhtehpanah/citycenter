d6a5506f804b399cd745f671faf335f2e74c2225
fakhtehpanah <fakhipanah@gmail.com>
2025-09-17 17:06:40 +0330

    create brands section

---
+++ src/components/sections/brands/Brands.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Brands() {
+  return (
+    <div className='flex justify-center bg-white/60 shadow-lg border border-dashed border-amber-200 backdrop-blur-sm py-10'>
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+    </div>
+  )
+}
+
+export default Brands
--- src/components/sections/product/Product.tsx
+++ src/components/sections/product/Product.tsx
@@ -4,6 +4,7 @@ import ProductCard from '../productCard/ProductCard';
 import type { Product } from '@/types/types';
 import Landing from '../landing/Landing';
 import LandingPage from '../landing/Landing';
+import Brands from '../brands/Brands';
 
 // interface ProductProps {
 //   product: Product;
@@ -46,9 +47,10 @@ function Product() {
 
     
   return (
-    <div className='bg-[#fff0d6] p-5'>
+    <div className='bg-[#fff0d6] p-5 flex flex-col gap-y-10'>
       {/* <Header setProductByCategory={setProductByCategory} /> */}
       <LandingPage/>
+      <Brands/>
       <div className="relative" >
                     <svg className='absolute  w-100 bottom-0 right-0 ' viewBox="0 0 50 100" ><path fill="rgba(251,236,255,1)" d="M96.5,58Q96,66,89,70.5Q82,75,76.5,78.5Q71,82,64.5,81.5Q58,81,52.5,84.5Q47,88,39,90.5Q31,93,29,84Q27,75,18.5,73.5Q10,72,12.5,64Q15,56,13.5,50Q12,44,13,37.5Q14,31,17.5,25Q21,19,26,13Q31,7,38.5,4.5Q46,2,53,5.5Q60,9,64.5,15Q69,21,72,25.5Q75,30,81,33.5Q87,37,92,43.5Q97,50,96.5,58Z"/></svg>
                     <svg className='absolute  w-100 top-0 right-0 ' viewBox="0 0 50 100" ><path fill="rgba(226,236,254,1)" d="M83,63Q71,76,56,82Q41,88,25,79Q9,70,7.5,49.5Q6,29,24.5,23.5Q43,18,60.5,16.5Q78,15,86.5,32.5Q95,50,83,63Z"/></svg> 
