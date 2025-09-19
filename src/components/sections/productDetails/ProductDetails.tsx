708c0b30ff6c0f3e28e0cae4f6684763c65eca62
fakhtehpanah <fakhipanah@gmail.com>
2025-09-19 23:04:40 +0330

    change product details styles

--- src/components/sections/productDetails/ProductDetails.tsx
+++ src/components/sections/productDetails/ProductDetails.tsx
@@ -14,9 +14,9 @@ export default function ProductDetails() {
     const fetchProduct = async () => {
       const allProducts = await getProductsItems();
       const found = allProducts.find((p: Product) => p.id.toString() === id);
-      console.log(product)
+      console.log(product);
       setProduct(found || null);
-    }
+    };
 
     fetchProduct();
   }, [id]);
@@ -24,13 +24,25 @@ export default function ProductDetails() {
   if (!product) return <div>Loading...</div>;
 
   return (
-      <div className="p-5 my-10 mx-5 flex flex-col lg:items-center gap-y-5  lg:p-10 ">
-        <h1 className="text-2xl  font-bold">{product.title}</h1>
-        <p className="text-green-700 font-bold">${product.price}</p>
-        <p className="text-gray-500 capitalize">{product.category}</p>
-        <img src="/food.JPG" alt={product.title} className="rounded lg:w-1/3" />
+    <div className="bg-[#fff0d6] relative flex flex-col justify-between p-5 lg:py-10">
+      <div className=" py-10 rounded shadow  flex flex-col lg:flex-row-reverse lg:justify-center lg:gap-x-10  gap-y-5 items-center ">
+        <img
+          src="/food.JPG"
+          alt={product.title}
+          className="rounded"
+          width={300}
+        />
+        <div className="flex flex-col gap-y-5">
+          <h1 className="text-2xl  font-bold">{product.title}</h1>
+          <p className="text-green-700 font-bold">${product.price}</p>
+          <p className="text-gray-500 capitalize">{product.category}</p>
+          <div>
+            <button className="border bg-black text-white hover:bg-white hover:text-black rounded cursor-pointer p-2 ">
+              add to Card
+            </button>
+          </div>
+        </div>
       </div>
-
-
+    </div>
   );
 }
