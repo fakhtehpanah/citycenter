420b960a5a7bc37447cf0b2010c8ad245f82941a
fakhtehpanah <fakhipanah@gmail.com>
2025-09-19 19:48:55 +0330

    change companies brands section styles

--- src/components/sections/brands/Brands.tsx
+++ src/components/sections/brands/Brands.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import { div } from 'three/tsl'
 
 function Brands() {
   return (
-    <div className='flex justify-center bg-white/60 shadow-lg border border-dashed border-amber-200 backdrop-blur-sm py-10'>
-        <p>1</p>
-        <p>2</p>
-        <p>3</p>
+    <div className=''>
+        <div className='grid grid-cols-3 gap-10 p-10 lg:flex lg:justify-center shadow bg-white/35  backdrop-blur-2xl'>
+        <img src="/google.png" width={100} alt="" />
+        <img src="/netflix.png" width={100} alt="" />
+          <img src="/shopify.png" width={100} alt="" />
+          <img src="/slack.png" width={100} alt="" />
+          <img src="/social.png" width={100} alt="" />
+          <img src="/spotify.png" width={100} alt="" />
     </div>
+
+    </div>
+
   )
 }
 
