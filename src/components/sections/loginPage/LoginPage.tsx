37426d7be2f63a7b8bad0a6230aeb1ab0c754ee7
fakhtehpanah <fakhipanah@gmail.com>
2025-09-30 18:05:16 +0330

    remove image from login page

--- src/components/sections/loginPage/LoginPage.tsx
+++ src/components/sections/loginPage/LoginPage.tsx
@@ -19,7 +19,7 @@ export default function LoginPage() {
           Login
         </h2>
 
-        <form className="space-y-6 z-1" onSubmit={handleLogin}>
+        <form className="space-y-6" onSubmit={handleLogin}>
           <div>
             <label htmlFor="email" className="block text-gray-700 py-2">
               Email
@@ -69,7 +69,7 @@ export default function LoginPage() {
           </a>
         </p>
       </div>
-      <img width={300} className="absolute top-0  left-20 py-5 " src="/Group.png" alt="" />
+      {/* <img width={300} className="absolute bottom-0  left-20 py-5 " src="/Group.png" alt="" /> */}
 
     </div>
     
